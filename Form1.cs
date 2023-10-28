feaab2a0f101ae3fb141506c40bc20fbb817470b
Phu <vandungqc03@gmail.com>
2023-10-28 18:29:12 +0700

    Finished eliminating 1 bit diff minterms and some minor changes
    Other changes:
    + func getNumbers_LineInputs()
      - Used Split and Parse approach to get number
    + Implemented operator overloading == for PrimeImplement class

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,3 @@
-using System.Linq;
-
 namespace WinFormsApp1
 {
 
@@ -26,6 +24,28 @@ namespace WinFormsApp1
                 isDontCare.Add(dontCare);
                 numbList.Add(value);
             }
+            public PrimeImplicant() { }
+
+            public static bool operator ==(PrimeImplicant obj1, PrimeImplicant obj2)
+            {
+                if (obj1.numbList.SequenceEqual(obj2.numbList) && obj1.isDontCare.SequenceEqual(obj2.isDontCare)
+                    && obj1.valueBin == obj2.valueBin && obj1.isCurrStageMatched == obj2.isCurrStageMatched
+                    && obj1.dashBin == obj2.dashBin)
+                    return true;
+                return false;
+            }
+
+            public static bool operator !=(PrimeImplicant obj1, PrimeImplicant obj2)
+            {
+                if (obj1.numbList.SequenceEqual(obj2.numbList) && obj1.isDontCare.SequenceEqual(obj2.isDontCare)
+                    && obj1.valueBin == obj2.valueBin && obj1.isCurrStageMatched == obj2.isCurrStageMatched
+                    && obj1.dashBin == obj2.dashBin)
+                    return false;
+                return true;
+            }
+
+            public bool setMatchStatus { set { isCurrStageMatched = value; } }
+            public int setDashPosition { set { dashBin = dashBin | (1 << value); } }
         }
 
         // Global variables
@@ -36,7 +56,7 @@ namespace WinFormsApp1
         Color normalColor = Color.White;
         Color successColor = Color.LimeGreen;
         Color errorColor = Color.Red;
-        string lineBreak = "--------------------------------------------------------------";
+        string lineBreak = "----------------------------------------------";
 
         private void display_Inputs(List<int> inputs)
         {
@@ -66,7 +86,7 @@ namespace WinFormsApp1
         private void check_CorrectNumberOfVariables()
         {
             int maxValue = inputVals[inputVals.Count - 1];
-            int maxBit = (int)(Math.Floor(Math.Log2(maxValue))) + 1;
+            int maxBit = get_BinaryLengthOfNumb(maxValue);
             if (maxBit != inputVariables.Count)
             {
                 string message =
@@ -87,33 +107,21 @@ namespace WinFormsApp1
 
         private void getNumbers_LineInputs(string line, ref List<int> currInputs)
         {
-            int currNumb = 0;
-            int currDigit = 1;
-            if (line.Length <= 0) throw new Exception("[ERROR]: O <gia_tri/dont_care> khong duoc de trong");
-            currInputs.Clear();
-            for (short i = 0; i < line.Length; i++)
+            try
             {
-                if (line[i] >= '0' && line[i] <= '9')
-                {
-                    currNumb = currNumb * currDigit + (line[i] - 48);
-                    currDigit = 10;
-                }
-                else if (line[i] == ',')
-                {
-                    currInputs.Add(currNumb);
-                    currNumb = 0;
-                    currDigit = 1;
-                }
-                else throw new Exception("[ERROR]: O <gia_tri/dont_care> khong dung format");
-
-                if (i + 1 >= line.Length && line[i] >= '0' && line[i] <= '9') currInputs.Add(currNumb);
+                String[] strList = line.Split(',');
+                foreach (String str in strList)
+                    currInputs.Add(int.Parse(str));
+            }
+            catch (Exception e)
+            {
+                throw new Exception("[ERROR]: O <gia_tri>/<dont_care> khong dung format/hoac dang de trong");
             }
         }
 
         private void getChar_LineInputs(string line, ref List<char> currChars)
         {
             if (line.Length <= 0) throw new Exception("[ERROR]: O <ten_bien> khong duoc de trong");
-            currChars.Clear();
             for (short i = 0; i < line.Length; i++)
             {
                 if (i % 2 == 0 && (line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] == 'Z'))
@@ -177,8 +185,13 @@ namespace WinFormsApp1
                 string lineValue = maskedTextBox1.Text.Trim();
                 string lineDontCare = maskedTextBox2.Text.Trim();
                 string lineVariable = maskedTextBox3.Text.Trim();
+                // Clear previous inputs
+                inputVals.Clear();
+                inputDontCares.Clear();
+                inputVariables.Clear();
+                // Get user entered inputs, <dont_care> input can be empty
                 getNumbers_LineInputs(lineValue, ref inputVals);
-                getNumbers_LineInputs(lineDontCare, ref inputDontCares);
+                if(lineDontCare.Length > 0) getNumbers_LineInputs(lineDontCare, ref inputDontCares);
                 getChar_LineInputs(lineVariable, ref inputVariables);
                 // Second checks if inputs are in the correct format to start using quine mccluskey algo
                 check_AscendingValsAndNoDupInput();
@@ -208,6 +221,11 @@ namespace WinFormsApp1
             }
         }
         
+        private int get_BinaryLengthOfNumb(int numb)
+        {
+            return (int)(Math.Floor(Math.Log2(numb))) + 1;
+        }
+
         private int count_OnesOfInputBinary(int value)
         {
             int count = 0;
@@ -233,6 +251,132 @@ namespace WinFormsApp1
             }
         }
 
+        private Dictionary<int, List<PrimeImplicant>> find_NextPrimeGroups(Dictionary<int, List<PrimeImplicant>> prevGroups, int maxBitLength)
+        {
+            Dictionary<int, List<PrimeImplicant>> newGroups = new Dictionary<int, List<PrimeImplicant>>();
+            List<PrimeImplicant> noMatchPrimes = new List<PrimeImplicant>();
+            foreach (KeyValuePair<int, List<PrimeImplicant>> prevItem in prevGroups)
+            {
+                foreach(PrimeImplicant currKeyObjPrime in prevItem.Value)
+                {
+                    if (!prevGroups.ContainsKey(prevItem.Key + 1)) break;
+                    foreach(PrimeImplicant cmpKeyObjPrime in prevGroups[prevItem.Key + 1])
+                    {
+                        PrimeImplicant dcCurrObjPrime = deepCopy_PrimeImplicant(currKeyObjPrime);
+                        PrimeImplicant dcCmpObjPrime = deepCopy_PrimeImplicant(cmpKeyObjPrime);
+                        update_CurrDcPrimeImplicantPair(ref dcCurrObjPrime, ref dcCmpObjPrime, maxBitLength);
+                        if (dcCurrObjPrime == currKeyObjPrime)
+                            continue;
+                        currKeyObjPrime.setMatchStatus = true;
+                        cmpKeyObjPrime.setMatchStatus = true;
+                        update_NewGroups(ref newGroups, ref dcCurrObjPrime, maxBitLength);
+                    }
+                }
+                // Check all prime obj that wasn't matched in this iteration and add them to newGroups Dict
+                for (short i = 0; i < prevItem.Value.Count; i++)
+                {
+                    PrimeImplicant prime = prevItem.Value[i];
+                    if (!prime.isCurrStageMatched)
+                        update_NewGroups(ref newGroups, ref prime, maxBitLength);
+                }
+            }
+            return newGroups;
+        }
+
+        private PrimeImplicant deepCopy_PrimeImplicant(PrimeImplicant primeObjToBeCopied)
+        {
+            PrimeImplicant newPrime = new PrimeImplicant();
+            // Perform manual copy
+            newPrime.valueBin = primeObjToBeCopied.valueBin;
+            newPrime.dashBin = primeObjToBeCopied.dashBin;
+            newPrime.isCurrStageMatched = primeObjToBeCopied.isCurrStageMatched;
+            // Loop through list
+            for(short i = 0; i < primeObjToBeCopied.numbList.Count; i++)
+            {
+                newPrime.numbList.Add(primeObjToBeCopied.numbList[i]);
+                newPrime.isDontCare.Add(primeObjToBeCopied.isDontCare[i]);
+            }
+            return newPrime;
+        }
+
+        private void update_CurrDcPrimeImplicantPair(ref PrimeImplicant dcCurrPrime, ref PrimeImplicant dcCmpPrime, int maxBitLength)
+        {
+            int newDashPosition = get_OneBitDiffPosition(dcCurrPrime.valueBin, dcCmpPrime.valueBin, 
+                dcCurrPrime.dashBin, dcCmpPrime.dashBin, maxBitLength);
+            if (newDashPosition != -1)
+            {
+                dcCurrPrime.setMatchStatus = true;
+                dcCurrPrime.setDashPosition = newDashPosition;
+                for (short i = 0; i < dcCmpPrime.numbList.Count; i++)
+                {
+                    dcCurrPrime.numbList.Add(dcCmpPrime.numbList[i]);
+                    dcCurrPrime.isDontCare.Add(dcCmpPrime.isDontCare[i]);
+                }
+            }
+        }
+
+        private int get_OneBitDiffPosition(int currValue, int cmpValue, int currDash, int cmpDash, int maxBitLength)
+        {
+            int shiftAmount = 0;
+            int countDiff = 0;
+            int diffPosition = 0;
+            if (currDash != cmpDash) return -1;
+            while (shiftAmount < maxBitLength)
+            {
+                int currValueShifted = (currValue >> shiftAmount) & 1;
+                int cmpValueShifted = (cmpValue >> shiftAmount) & 1;
+                if(currValueShifted != cmpValueShifted)
+                {
+                    countDiff += 1;
+                    diffPosition = shiftAmount;
+                }
+                shiftAmount += 1;
+            }
+            return countDiff == 1 ? diffPosition : -1;
+        }
+
+        private void update_NewGroups(ref Dictionary<int, List<PrimeImplicant>> newGroups, ref PrimeImplicant newPrime, int maxBitLength)
+        {
+            int newDcCurrObjValueBin = remove_ValueBinBitBasedOnDashBin(newPrime.valueBin, newPrime.dashBin, maxBitLength);
+            int newKey = count_OnesOfInputBinary(newDcCurrObjValueBin);
+            add_PrimeToNextGroups(ref newGroups, ref newPrime, newKey);
+        }
+
+        private int remove_ValueBinBitBasedOnDashBin(int currPrimeValBin, int currPrimeDashBin, int maxBitLength)
+        {
+            for(int shiftAmount = 0; shiftAmount < maxBitLength; shiftAmount++)
+            {
+                int lsbOfDashBin = (currPrimeDashBin >> shiftAmount) & 1;
+                if (lsbOfDashBin == 1) currPrimeValBin &= ~(1 << shiftAmount);
+            }
+            return currPrimeValBin;
+        }
+
+        private void add_PrimeToNextGroups(ref Dictionary<int, List<PrimeImplicant>> newGroups, ref PrimeImplicant newPrime, int newKey) 
+        {
+            if (!newGroups.ContainsKey(newKey))
+                newGroups.Add(newKey, new List<PrimeImplicant>());
+            newGroups[newKey].Add(newPrime);
+        }
+
+        private bool check_GroupsEquality(ref Dictionary<int, List<PrimeImplicant>> group1, ref Dictionary<int, List<PrimeImplicant>> group2)
+        {
+            if (group1.Count != group2.Count) return false;
+            foreach (int keyGroup1 in group1.Keys)
+                if (!group2.ContainsKey(keyGroup1)) return false;
+            foreach(KeyValuePair<int, List<PrimeImplicant>> item in group1)
+                if (group2[item.Key].Count != item.Value.Count) return false;
+            return true;
+        }
+
+        private Dictionary<int, List<PrimeImplicant>> clear_PrimeMatchStatusInGroups(Dictionary<int, List<PrimeImplicant>> groups)
+        {
+            foreach(List<PrimeImplicant> primeList in groups.Values)
+                foreach (PrimeImplicant prime in primeList)
+                    prime.setMatchStatus = false;
+            return groups;
+        }
+
         private void main_QuineMcCluskeyAlgo()
         {
             // Sorting
@@ -243,9 +387,22 @@ namespace WinFormsApp1
             // Main
             Dictionary<int, List<PrimeImplicant>>groups = new Dictionary<int, List<PrimeImplicant>>();
             arrange_InputGroup(groups);
+            int maxNumbBitLength = get_BinaryLengthOfNumb(inputVals[inputVals.Count - 1]);
+            int iteration = 0;
+            while (true)
+            {
+                Dictionary<int, List<PrimeImplicant>> newGroups = find_NextPrimeGroups(groups, maxNumbBitLength);
+                if (check_GroupsEquality(ref newGroups, ref groups)) break;
+                groups = clear_PrimeMatchStatusInGroups(newGroups);
+                display_Result($"Lan lap thu: {iteration + 1}");
+                iteration++;
+                if (iteration >= 10)
+                {
+                    display_Result("[ERROR] Experiment infinity loop");
+                    break;
+                }
+            }
             display_PrimeGroups(groups);
-            //PrimeImplicant obj = new PrimeImplicant(10, true);
-            //display_Prime(obj);
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
